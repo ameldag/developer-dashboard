dc7e7f056d96b882c646b094f19df4f45c80fa86
Slim Ben Nasrallah <slimbennasrallah@gmail.com>
2020-05-14 01:29:14 +0200

    all the dialogs are in between comments

--- src/views/pages/Statements.vue
+++ src/views/pages/Statements.vue
@@ -22,8 +22,13 @@
 
 			</el-col>
 		</el-row>
-		<el-dialog :visible.sync="FormVisible" width="30%" custom-class="dialog" center>
+
+
+		<!-- DIALOG -->
+
+		<!-- <el-dialog :visible.sync="FormVisible" width="30%" custom-class="dialog" center>
 			<el-form label-position="top" ref="accountForm" :model="accountForm" :rules="rules">
+				
 
 				<el-form-item label="ACCOUNT HOLDER NAME:" prop="holder_name">
 					<el-input type="text" v-model="accountForm.holder_name" />
@@ -62,7 +67,47 @@
 				</div>
 				
 			</el-form>
+		</el-dialog> -->
+
+		<!-- DIALOG END -->
+
+
+		<!-- VERIFY YOUR ACCOUNT DIALOG -->
+<!-- 
+		<el-dialog :visible.sync="FormVisible" width="30%" custom-class="dialog" center>
+			<div class="grid grid-cols-1 text-gray-100 text-center">
+				<h2 class="text-2xl font-bold mb-10">Verify your account</h2>
+
+				<p class="mb-16">Complete the process of verification through the Stripe secured platform</p>
+
+				<svg class="w-24 h-auto" fill="#4379FF" viewBox="0 0 62 26">
+					<path
+						d="M5 10.1c0-.6.6-.9 1.4-.9 1.2 0 2.8.4 4 1.1V6.5c-1.3-.5-2.7-.8-4-.8C3.2 5.7 1 7.4 1 10.3c0 4.4 6 3.6 6 5.6 0 .7-.6 1-1.5 1-1.3 0-3-.6-4.3-1.3v3.8c1.5.6 2.9.9 4.3.9 3.3 0 5.5-1.6 5.5-4.5.1-4.8-6-3.9-6-5.7zM29.9 20h4V6h-4v14zM16.3 2.7l-3.9.8v12.6c0 2.4 1.8 4.1 4.1 4.1 1.3 0 2.3-.2 2.8-.5v-3.2c-.5.2-3 .9-3-1.4V9.4h3V6h-3V2.7zm8.4 4.5L24.6 6H21v14h4v-9.5c1-1.2 2.7-1 3.2-.8V6c-.5-.2-2.5-.5-3.5 1.2zm5.2-2.3l4-.8V.8l-4 .8v3.3zM61.1 13c0-4.1-2-7.3-5.8-7.3s-6.1 3.2-6.1 7.3c0 4.8 2.7 7.2 6.6 7.2 1.9 0 3.3-.4 4.4-1.1V16c-1.1.6-2.3.9-3.9.9s-2.9-.6-3.1-2.5H61c.1-.2.1-1 .1-1.4zm-7.9-1.5c0-1.8 1.1-2.5 2.1-2.5s2 .7 2 2.5h-4.1zM42.7 5.7c-1.6 0-2.5.7-3.1 1.3l-.1-1h-3.6v18.5l4-.7v-4.5c.6.4 1.4 1 2.8 1 2.9 0 5.5-2.3 5.5-7.4-.1-4.6-2.7-7.2-5.5-7.2zm-1 11c-.9 0-1.5-.3-1.9-.8V10c.4-.5 1-.8 1.9-.8 1.5 0 2.5 1.6 2.5 3.7 0 2.2-1 3.8-2.5 3.8z">
+					</path>
+				</svg>
+
+				<button class="bg-blue-600 rounded-md py-3 w-full mt-20">Go to verification</button>
+			</div>
+		</el-dialog> -->
+
+		<!-- VERIFY YOUR ACCOUNT DIALOG END -->
+
+		<!-- PROFILE VERIFIED DIALOG -->
+
+		<el-dialog :visible.sync="FormVisible" width="30%" custom-class="dialog" center>
+			<div class="grid grid-cols-1 text-gray-100 text-center">
+				<h2 class="text-2xl font-bold mb-10">Verify your account</h2>
+
+				<p class="mb-16">Profile verified.</p>
+
+				<button class="bg-blue-600 rounded-md py-3 w-full mt-20">Go to verification</button>
+			</div>
 		</el-dialog>
+
+		<!-- PROFILE VERIFIED DIALOG END -->
+
+
+
 		<el-row :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
 			<el-col :xs="24" :sm="12" :md="12" :lg="8" :xl="8" style='margin: auto;	float: none; text-align: center;'>
 
@@ -70,8 +115,9 @@
 					v-if="this.$store.state.session.user.payment_account_id">
 					Withdraw
 				</button>
-				
-				<el-button type="primary" class="uppercase bg-teal-600 font-bold" round @click="FormVisible = true" v-else>Create Account
+
+				<el-button type="primary" class="uppercase bg-teal-600 font-bold" round @click="FormVisible = true"
+					v-else>Create Account
 				</el-button>
 			</el-col>
 		</el-row>
